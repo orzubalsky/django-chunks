04c76bb263643a741436ff93bef5e54317ea733f
orzubalsky <juviley@gmail.com>
2012-12-27 07:59:18 -0500

    adding site to token in  template tags

--- chunks/templatetags/chunks.py
+++ chunks/templatetags/chunks.py
@@ -10,19 +10,20 @@ CACHE_PREFIX = "chunk_"
 def do_chunk(parser, token):
     # split_contents() knows not to split quoted strings.
     tokens = token.split_contents()
-    if len(tokens) < 2 or len(tokens) > 3:
-        raise template.TemplateSyntaxError, "%r tag should have either 2 or 3 arguments" % (tokens[0],)
-    if len(tokens) == 2:
-        tag_name, key = tokens
-        cache_time = 0
+    if len(tokens) < 3 or len(tokens) > 4:
+        raise template.TemplateSyntaxError, "%r tag should have either 3 or 4 arguments" % (tokens[0],)
     if len(tokens) == 3:
-        tag_name, key, cache_time = tokens
+        tag_name, key, site = tokens
+        cache_time = 0
+    if len(tokens) == 4:
+        tag_name, key, site, cache_time = tokens
     key = ensure_quoted_string(key, "%r tag's argument should be in quotes" % tag_name)
-    return ChunkNode(key, cache_time)
+    return ChunkNode(key, site, cache_time)
 
 class ChunkNode(template.Node):
-    def __init__(self, key, cache_time=0):
+    def __init__(self, key, site, cache_time=0):
        self.key = key
+       self.site = site
        self.cache_time = cache_time
 
     def render(self, context):
@@ -30,7 +31,7 @@ class ChunkNode(template.Node):
             cache_key = CACHE_PREFIX + self.key
             c = cache.get(cache_key)
             if c is None:
-                c = Chunk.objects.get(key=self.key)
+                c = Chunk.objects.get(key=self.key, site=self.site)
                 cache.set(cache_key, c, int(self.cache_time))
             content = c.content
         except Chunk.DoesNotExist:
@@ -42,13 +43,14 @@ def do_get_chunk(parser, token):
     tokens = token.split_contents()
     if len(tokens) != 4 or tokens[2] != 'as':
         raise template.TemplateSyntaxError, 'Invalid syntax. Usage: {%% %s "key" as varname %%}' % tokens[0]
-    tagname, key, varname = tokens[0], tokens[1], tokens[3]
+    tagname, key, site, varname = tokens[0], tokens[1], tokens[2], tokens[4]
     key = ensure_quoted_string(key, "Key argument to %r must be in quotes" % tagname)
-    return GetChunkNode(key, varname)
+    return GetChunkNode(key, site, varname)
 
 class GetChunkNode(template.Node):
-    def __init__(self, key, varname):
+    def __init__(self, key, site, varname):
         self.key = key
+        self.site = site
         self.varname = varname
 
     def render(self, context):
